3f1d16c596ff4a00c92ec3a027b3f268dcfe0210
freeonisland <freeonisland@yahoo.fr>
2020-04-04 16:11:37 +0200

    ldap manager

--- src/Ldap/Controller/IndexController.php
+++ src/Ldap/Controller/IndexController.php
@@ -3,6 +3,8 @@
 namespace Ldap\Controller;
 
 use App\Controller\AbstractController;
+use Ldap\Manager\SimpleManager;
+use Ldap\Manager\LaminasManager;
 
 class IndexController extends AbstractController
 {
@@ -11,17 +13,7 @@ class IndexController extends AbstractController
      */
     public function indexAction()
     {
-        $ldap_conn = ldap_connect('ldap://' . $this->getParam('LDAP_SERVER'));
-        $ldap_dn = $this->getParam('LDAP_DN');
-        $ldap_pass = $this->getParam('LDAP_PASSWORD');
-
-        try {
-            if ($d=ldap_bind_ext($ldap_conn, $ldap_dn, $ldap_pass, [['oid' => LDAP_CONTROL_PASSWORDPOLICYREQUEST]] )) {
-                echo 'LDAP connected';
-                s($d);
-            } else {
-                echo "Can't connect to LDAP";
-            }
-        } catch(\ErrorException $e) { echo "LDAP EXCEPTION: ".$e->getMessage(); }
+        $simpleManager = new SimpleManager($this->getParam('LDAP_SERVER'), $this->getParam('LDAP_DN'), $this->getParam('LDAP_PASSWORD'));
+        $simpleManager->connect();
     }
 }
--- src/Ldap/Manager/SimpleManager.php
+++ src/Ldap/Manager/SimpleManager.php
@@ -4,5 +4,28 @@ namespace Ldap\Manager;
 
 class SimpleManager
 {
-    
+    private $ldapServer;
+    private $ldapDn;
+    private $ldapPass;
+
+    public function __construct($ldap_server, $ldap_dn, $ldap_pass)
+    {
+        $this->ldapServer = $ldap_server;
+        $this->ldapDn = $ldap_dn;
+        $this->ldapPass = $ldap_pass;
+    }
+
+    public function connect()
+    {
+        $ldap_conn = ldap_connect('ldap://' . $this->ldapServer);
+
+        try {
+            if ($d=ldap_bind_ext($ldap_conn, $this->ldapDn, $this->ldapPass, [['oid' => LDAP_CONTROL_PASSWORDPOLICYREQUEST]] )) {
+                echo 'LDAP connected.';
+                s($d);
+            } else {
+                echo "Can't connect to LDAP";
+            }
+        } catch(\ErrorException $e) { echo "LDAP EXCEPTION: ".$e->getMessage(); }
+    }
 }
